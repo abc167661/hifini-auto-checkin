f3ca44aff75ad4a1a79d79faa31a68e796119140
abc167661 <108221120+abc167661@users.noreply.github.com>
2026-05-25 21:04:20 +0800

    Create mian1.js

---
+++ mian1.js
@@ -0,0 +1,102 @@
+import { appendFileSync } from "fs";
+
+const signPageUrl = "https://www.hifiki.com/sg_sign.htm";
+const responseSuccessCode = "0";
+
+async function checkIn(account) {
+  console.log(`【${account.name}】: 开始签到...`);
+
+  const response = await fetch(signPageUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "X-Requested-With": "XMLHttpRequest",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+      Cookie: account.cookie,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`网络请求出错 - ${response.status}`);
+  }
+
+  const responseJson = await response.json();
+
+  if (responseJson.code === responseSuccessCode) {
+    console.log(`【${account.name}】: 签到成功。`);
+    return responseJson.message;
+  } else {
+    if (responseJson.message === "今天已经签过啦！") {
+      console.log(`【${account.name}】: ${responseJson.message}`);
+      return responseJson.message;
+    }
+    throw new Error(`签到失败: ${responseJson.message}`);
+  }
+}
+
+// 处理
+async function processSingleAccount(account) {
+  const checkInResult = await checkIn(account);
+
+  return checkInResult;
+}
+
+function setGitHubOutput(name, value) {
+  appendFileSync(process.env.GITHUB_OUTPUT, `${name}<<EOF\n${value}\nEOF\n`);
+}
+
+async function main() {
+  let accounts;
+
+  try {
+    if (!process.env.ACCOUNTS) {
+      throw new Error("❌ 未配置账户信息。");
+    }
+
+    accounts = JSON.parse(process.env.ACCOUNTS);
+  } catch (error) {
+    const message = `❌ ${error.message.includes("JSON") ? "账户信息配置格式错误。" : error.message
+      }`;
+    console.error(message);
+    setGitHubOutput("result", message);
+    process.exit(1);
+  }
+
+  const allPromises = accounts.map((account) => processSingleAccount(account));
+  const results = await Promise.allSettled(allPromises);
+
+  const msgHeader = "\n======== 签到结果 ========\n\n";
+  console.log(msgHeader);
+
+  let hasError = false;
+
+  const resultLines = results.map((result, index) => {
+    const accountName = accounts[index].name;
+
+    const isSuccess = result.status === "fulfilled";
+
+    if (!isSuccess) {
+      hasError = true;
+    }
+
+    const icon = isSuccess ? "✅" : "❌";
+    const message = isSuccess ? result.value : result.reason.message;
+
+    const line = `${accountName}: ${icon} ${message}`;
+
+    isSuccess ? console.log(line) : console.error(line);
+
+    return line;
+  });
+
+  const resultMsg = resultLines.join("\n");
+
+  setGitHubOutput("result", resultMsg);
+
+  if (hasError) {
+    process.exit(1);
+  }
+}
+
+main();
